21dc95d75343f584c7c827745110984715825582
RezaHemadi <r.hemadi@gmail.com>
2023-05-29 16:57:05 +0330

    Initial Commit

---
+++ RadialColorPicker/RadialColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadialColorPickerApp.swift
+//  RadialColorPicker
+//
+//  Created by Reza on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct RadialColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
